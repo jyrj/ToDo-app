a107374b4ad48c46d827da6e9a0153a59a260a94
jyrj <jayarajevur@gmail.com>
2020-08-15 00:44:27 +0530

    master

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 from flask import Flask, render_template, request, url_for, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
@@ -18,13 +19,23 @@ db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
-    description = request.get_json()['description']
-    todo = Todo(description=description)
-    db.session.add(todo)
-    db.session.commit()
-    return jsonify ({
-        'description': todo.description
-    })
+    error= False
+    body = {}
+    try:
+        description = request.get_json()['description']
+        todo = Todo(description=description)
+        db.session.add(todo)
+        db.session.commit()
+        body['description'] = todo.description
+
+    except:
+        db.session.rollback()
+        error= True
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if not error:
+        return jsonify (body)
 
 
 
